521dfc27cdee7f525269e9cc36bcd29eb9e5c0c9
Paritosh Chaudhary <51153681+Paritosh008@users.noreply.github.com>
2023-04-16 15:46:06 +0530

    Create Ques-2.js

---
+++ Ques-2.js
@@ -0,0 +1,11 @@
+// 2. Apply following style to the same h2 using Javascript DOM
+// Method.
+// colour: red,
+// fontWeight: bold,
+// textTransform: uppercase
+
+//Solution
+const h2 = document.querySelector('#text');
+h2.style.color = 'red';
+h2.style.fontWeight = 'bold';
+h2.style.textTransform = 'uppercase';
